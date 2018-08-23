0df6c6a35bde3bcd1eee13b6a5338bb3ac00ccf5
Peter Monks <pmonks@gmail.com>
2018-08-22 18:46:14 -0700

    :arrow_up: Upgrade dependencies

--- project.clj
+++ project.clj
@@ -14,19 +14,19 @@
   :license {:name "Creative Commons Attribution-ShareAlike 3.0 Unported License."
             :url "http://creativecommons.org/licenses/by-sa/3.0/"}
   :dependencies [
-                  [org.clojure/clojure            "1.8.0"]
-                  [org.clojure/tools.cli          "0.3.3"]
+                  [org.clojure/clojure            "1.9.0"]
+                  [org.clojure/tools.cli          "0.3.7"]
                   [org.clojure/tools.trace        "0.7.9"]
-                  [org.clojure/tools.logging      "0.3.1"]
-                  [instaparse                     "1.4.1"]
-                  [ch.qos.logback/logback-classic "1.1.3"]
+                  [org.clojure/tools.logging      "0.4.1"]
+                  [instaparse                     "1.4.9"]
+                  [ch.qos.logback/logback-classic "1.2.3"]
                 ]
   :profiles {:dev {:dependencies [
-                                   [midje "1.8.3"]
+                                   [midje "1.9.2"]
                                    [clj-ns-browser "1.3.1"]
                                  ]
                    :plugins [
-                              [lein-midje "3.2"]
+                              [lein-midje "3.2.1"]
                             ]}}
   :jvm-opts ^:replace []  ; Stop Leiningen from turning off JVM optimisations - makes it slower to start but ensures code runs as fast as possible
   :main lolcmis.core)
